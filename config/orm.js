967e67bf24e65758322787fdd1a421f2aa3cef2e
caglarpanus <panuscaglar@gmail.com>
2018-03-19 14:08:07 -0400

    orm is done

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,29 @@
-var connection = require('../config/connection.js');
+var connection = require("./connection.js");
+
+var orm = {
+    getAll: function(cb){
+        connection.query("SELECT * FROM burgers", function(err,data){
+            if(err) throw err;
+            console.log(data);
+            cb(data);
+        });
+    },
+
+    add: function(name,cb){
+        var query = "INSERT INTO burgers(name,devoured) VALUES(?,)";
+
+        connection.query(query,[name],function(err,data){
+            if(err) throw err;
+            console.log(data);
+            cb(data);
+        });
+    },
+    update: function(id,cb){
+        var query = "UPDATE burgers SET devoured = 1 WHERE id = ?";
+        connection.query(query,[id], function(err,data){
+            if(err) throw err;
+            console.log(data);
+            cb(data);
+        });
+    }
+};
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.engine('handlebars', exphbs({defaultLayout:"main"}));
+app.set('view engine', 'handlebars');
+
+const router = require("./controllers/burgersController.js");
+app.use(router);
+
+app.listen(PORT, function() {
+    console.log("Listening on port " + PORT);
+});
