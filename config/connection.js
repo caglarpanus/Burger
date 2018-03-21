d9b995722a6ad52dee2d4d7e0b8bcac6692917bd
caglarpanus <panuscaglar@gmail.com>
2018-03-21 10:57:16 -0400

    JawsDB added to configuratio

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 const mysql = require("mysql");
 require('dotenv').config();
+var connection;
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE
-});
+//Please see using JawsDB with Node.js. This had to be connected this way because of deployment to Heroku.
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_DATABASE
+    });
+}
+
 
 connection.connect(function(err){
     if(err){
